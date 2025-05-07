e5facc417f562dca445364da3d886f5f6ca17c8a
kimjin1022 <jinjinjara1022@naver.com>
2025-05-07 10:27:31 +0000

    upgrade mini_max

--- backend/app/strategies/mini_max_ai.py
+++ backend/app/strategies/mini_max_ai.py
@@ -3,31 +3,31 @@ import copy
 
 def evaluate_board(board, player):
     score = 0
-    opponent = 1 if player == 2 else 2
     for i in range(19):
         for j in range(19):
             if j <= 14:
                 pattern = board[i, j:j + 5]
-                score += get_score_from_pattern(pattern, player, opponent)
+                score += get_score_from_pattern(pattern, player)
             if i <= 14:
                 pattern = board[i:i + 5, j]
-                score += get_score_from_pattern(pattern, player, opponent)
+                score += get_score_from_pattern(pattern, player)
             if i <= 14 and j <= 14:
                 pattern = np.diag(board[i:i + 5, j:j + 5])
-                score += get_score_from_pattern(pattern, player, opponent)
+                score += get_score_from_pattern(pattern, player)
             if i <= 14 and j >= 4:
                 pattern = np.diag(np.fliplr(board[i:i + 5, j - 4:j + 1]))
-                score += get_score_from_pattern(pattern, player, opponent)
+                score += get_score_from_pattern(pattern, player)
     return score
 
-def get_score_from_pattern(pattern, player, opponent):
+def get_score_from_pattern(pattern, player):
     score = 0
+    opponent = 1 if player == 2 else 2
     player_count = pattern.tolist().count(player)
     opponent_count = pattern.tolist().count(opponent)
 
-    if opponent_count == 0:
+    if opponent_count == 0 and player == 1:
         if player_count == 5:
-            score += 100000
+            score += 1000000
         elif player_count == 4:
             score += 10000
         elif player_count == 3:
@@ -37,22 +37,22 @@ def get_score_from_pattern(pattern, player, opponent):
         elif player_count == 1:
             score += 10
     
-    elif player_count == 0:
-        if opponent_count == 5:
-            score -= 150000
-        elif opponent_count == 4:
-            score -= 15000 
-        elif opponent_count == 3:
-            score -= 1500
-        elif opponent_count == 2:
-            score -= 150   
-        elif opponent_count == 1:
-            score -= 15 
+    elif opponent_count == 0 and player == 2:
+        if player_count == 5:
+            score -= 100000
+        elif player_count == 4:
+            score -= 10000
+        elif player_count == 3:
+            score -= 1000
+        elif player_count == 2:
+            score -= 100
+        elif player_count == 1:
+            score -= 10
 
     return score
 
+
 def get_mini_max_move(board):
-    board = np.array(board)
     able_positions = set()
     directions = [(1, 0), (0, 1), (1, 1), (1, -1), (-1, 0), (0, -1), (-1, -1), (-1, 1)]
 
@@ -78,7 +78,7 @@ def get_mini_max_move(board):
             for j in range(19):
                 if new_board[i, j] != 0:
                     for dx, dy in directions:
-                        for dist in range(1, 2):
+                        for dist in range(1, 3):
                             nx, ny = i + dx * dist, j + dy * dist
                             if 0 <= nx < 19 and 0 <= ny < 19 and new_board[nx, ny] == 0:
                                 able_positions2.add((nx, ny))
